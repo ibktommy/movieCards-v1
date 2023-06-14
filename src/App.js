4d2d0dc347c2bdca5ccf80bace8978221ed50310
ibktommy <atomquartz@gmail.com>
2023-06-14 17:58:04 +0100

    Fixing useEffect bug for netlify upload

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import Home from "./components/Home";
 import Loading from "./components/Loading";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(false);
+
 	const apiURL =
 		"https://api.themoviedb.org/3/movie/popular?api_key=30b4ef3f8b7fd7bcf7870dddc7c9a135&language=en-US&page=1";
 
-	const [movies, setMovies] = useState([]);
-	const [loading, setLoading] = useState(false);
+    async function fetchMovies() {
+			setLoading(true);
+			try {
+				const response = await fetch(apiURL);
+				const responseData = (await response.json()).results;
 
-	async function fetchMovies() {
-		setLoading(true);
-
-		try {
-			const response = await fetch(apiURL);
-			const responseData = (await response.json()).results;
-
-			setMovies(responseData);
-			setLoading(false);
-			console.log(movies);
-		} catch (error) {
-			console.log(error);
+				setMovies(responseData);
+				setLoading(false);
+				console.log(movies);
+			} catch (error) {
+				console.log(error);
+			}
 		}
-	}
+
 
 	useEffect(() => {
 		fetchMovies();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	if (loading) {
