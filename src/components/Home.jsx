6d2998dbf81e0bd34c8e859309f06b92fcc4a691
ibktommy <atomquartz@gmail.com>
2023-06-14 19:22:37 +0100

    change the text-content of the main-header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import MovieCards from "./MovieCards";
 const Home = ({ moviesDataProps, deleteMovie }) => {
 	return (
 		<div className="main">
-			<h1 className="header">movies</h1>
+			<h1 className="header">popular movies</h1>
 			<MovieCards moviesDataProps={moviesDataProps} deleteMovie={deleteMovie} />
 		</div>
 	);
